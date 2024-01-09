724baafe5ddb20d7e83b7531deef247e04b36f3c
Jaredglass3 <Jaredglass352@gmail.com>
2024-01-09 11:40:44 -0600

    Built layout of buissiness card

---
+++ unity_ar_buisiness_card/Assets/Scripts/LinkedInButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LinkedInButton : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        // Handle LinkedIn button click action here
+        // For example, open a LinkedIn URL
+        Application.OpenURL("https://www.linkedin.com/in/jared-glass-0623k/");
+    }
+}
